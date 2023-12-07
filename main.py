6e4fdc9ffa05602d45c3210790b6e039bc07b662
Anna J <ajasion5@gmail.com>
2023-12-07 23:59:45 +0000

    add if/elif statemn for each math operator-correct

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ Please type in the math operation you would like to complete:
 num_1 = int(input('Enter your first number: '))
 num_2 = int(input('Enter your second number: '))
 
-# Addition
-print('{} + {} = '.format(num_1, num_2))
-print(num_1 + num_1)
+if math_operation == '+':
+    print('{} + {} = '.format(num_1, num_2))
+    print(num_1 + num_2)
 
-# Subtraction
-print('{} - {} = '.format(num_1, num_2))
-print(num_1 - num_2)
+elif math_operation == '-':
+    print('{} - {} = '.format(num_1, num_2))
+    print(num_1 - num_2)
 
-# Multiplication
-print('{} * {} = '.format(num_1, num_2))
-print(num_1 * num_2)
+elif math_operation == '*':
+    print('{} * {} = '.format(num_1, num_2))
+    print(num_1 * num_2)
 
-# Division
-print('{} / {} = '.format(num_1, num_2))
-print(num_1 / num_2)
+elif math_operation == '/':
+    print('{} / {} = '.format(num_1, num_2))
+    print(num_1 / num_2)
