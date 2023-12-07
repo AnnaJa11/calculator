5b6ef2e9e70cd9ee54d9e07da11057797e73229a
Anna J <ajasion5@gmail.com>
2023-12-07 23:57:24 +0000

    add if/elif statements for each math operator

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+
+math_operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
 num_1 = int(input('Enter your first number: '))
 num_2 = int(input('Enter your second number: '))
 
