143f14ac4661f2e27cc8bcaa99e5fb0b8dd448e0
Anna J <ajasion5@gmail.com>
2023-12-18 08:10:33 +0000

    add prompt_imput function

--- main.py
+++ main.py
@@ -2,28 +2,58 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-math_operation = input('''
-Please type in the math operation you would like to complete:
-+ for addition
-- for subtraction
-* for multiplication
-/ for division
-''')
-num_1 = int(input('Enter your first number: '))
-num_2 = int(input('Enter your second number: '))
+# Add 2 numbers
+def add(num_1,num_2):
+    return(num_1 + num_2)
+# Subtract 2 numbers
+def subtract(num_1,num_2):
+    return(num_1 - num_2)
+# Multiply 2 numbers
+def multiply(num_1,num_2):
+    return(num_1 * num_2)
+# Divide 2 numbers
+def divide(num_1,num_2):
+    return(num_1 / num_2)
 
-if math_operation == '+':
-    logging.info('{} + {} = '.format(num_1, num_2))
-    print(num_1 + num_2)
+def prompt_input():
+    num_1 = float(input('Enter your first number: '))
+    num_2 = float(input('Enter your second number: '))
+    
+    select = int(input('''
+        Please select operation:
+        1 for addition
+        2 for subtraction
+        3 for multiplication
+        4 for division
+        '''))
+    return(num_1,num_2,select)
 
-elif math_operation == '-':
-    logging.info('{} - {} = '.format(num_1, num_2))
-    print(num_1 - num_2)
+continue_calculating = True
+while continue_calculating is True:
+    num_1, num_2, select = prompt_input()
+    
+    if select == 1:
+        logging.info('Dodaje {} i {}. Wynik to '.format(num_1, num_2))
+        print(num_1 + num_2)
 
-elif math_operation == '*':
-    logging.info('{} * {} = '.format(num_1, num_2))
-    print(num_1 * num_2)
+    elif select == 2:
+            logging.info('{} - {} = '.format(num_1, num_2))
+            print(num_1 - num_2)
 
-elif math_operation == '/':
-    logging.info('{} / {} = '.format(num_1, num_2))
-    print(num_1 / num_2)
+    elif select == 3:
+            logging.info('{} * {} = '.format(num_1, num_2))
+            print(num_1 * num_2)
+
+    elif select == 4:
+        try:
+            logging.info('{} / {} = '.format(num_1, num_2))
+            print(num_1 / num_2)
+        except:
+            print("Divsion by 0 not possible!")
+    else:
+            logging.warning('Invalid operator')
+
+    want_continue = input('Continue? y/n: ').lower()
+    if want_continue == "n":
+        logging.info('Closing program...')
+        continue_calculating = False
